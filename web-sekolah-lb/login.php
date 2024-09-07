5b8dd52fe685997d6c9848caee144a01553ccd75
Falathin Nakamura <ibrahimahmadfalatin@gmail.com>
2024-09-07 14:01:26 +0700

    Menambahkan Halaman Login Level Memakai PHP.

---
+++ web-sekolah-lb/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>INVENTARIS</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 
+	<?php
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan']=="gagal"){
+			echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+		}
+	}
+	?>
+	<div class="kotak_login">
+		<div class=img1><img src="asset/img/logoSmkLb.png" width=80px height=80px></div>	
+			<p class="tulisan_login"><b>Silahkan login</p><br>
+
+		<form action="cek_login.php" method="post">
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
+ 
+			<label>Password</label>
+			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+ 
+			<input type="submit" class="tombol_login" value="LOGIN">
+ 
+			<br/>
+			<br/>
+		</form>
+		
+	</div>
+ 
+ 
+</body>
+</html>
