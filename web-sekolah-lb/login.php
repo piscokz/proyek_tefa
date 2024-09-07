ef6a4951f9fc717427840fa1a6db541bbe1328f1
Falathin Nakamura <ibrahimahmadfalatin@gmail.com>
2024-09-07 14:05:32 +0700

    Menambahkan CSS Login.

--- web-sekolah-lb/login.php
+++ web-sekolah-lb/login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>INVENTARIS</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 <body>
  
