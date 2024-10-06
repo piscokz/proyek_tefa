6f6415e29b281ac4c12e7a759fab87f0a2abf69b
Falathin Nakamura <ibrahimahmadfalatin@gmail.com>
2024-10-06 15:55:02 +0700

    Menambahkan fitur notifikasi  dan icon merah

--- testLaravel/resources/views/partials/notification.blade.php
+++ testLaravel/resources/views/partials/notification.blade.php
@@ -1,7 +1,9 @@
 <li class="nav-item dropdown">
     <a class="nav-link count-indicator" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
         <i class="icon-bell"></i>
-        <span class="count">{{ $contacts->count() }}</span> <!-- Menampilkan jumlah notifikasi -->
+        @if ($contacts->count() > 0)
+            <span class="count">{{ $contacts->count() }}</span> <!-- Menampilkan jumlah notifikasi -->
+        @endif
     </a>
     <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0" aria-labelledby="notificationDropdown">
         <a class="dropdown-item py-3 border-bottom">
